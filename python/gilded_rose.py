6a25e671c8e42e5f66cc6ac8c650edc73d41f943
Adam Lear <adamlear12@gmail.com>
2024-05-30 23:59:28 -0500

    fix conditional for backstage passes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -28,14 +28,12 @@ class GildedRose(object):
         item.quality += 1
 
     def update_backstage_passes(self, item):
-        if item.sell_in == 0:
+        if item.sell_in <= 0:
             item.quality = 0
         elif item.sell_in <= 5:
             item.quality += 3
         elif item.sell_in <= 10:
             item.quality += 2
-        else:
-            item.quality += 1
         item.sell_in -= 1
         self.handle_limits(item)
 
